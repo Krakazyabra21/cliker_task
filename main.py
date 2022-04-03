8fcc053f52d5dc542eaa534c281460eb89c32eb7
Krakazyabra21 <krakazyabra2102@mail.ru>
2022-04-03 13:42:29 +0300

    menu

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 import sys
 
-from PyQt5.QtCore import Qt
+from PyQt5 import QtWidgets, QtGui
 from PyQt5.QtWidgets import QApplication, QMainWindow
+
+class Window(QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setWindowTitle("Игрушка")
+        self.setGeometry(300, 250, 800, 600)
+
+        self.main_text = QtWidgets.QLabel(self)
+        self.main_text.setText("Кликомания")
+        self.main_text.setFont(QtGui.QFont("Times", 32, QtGui.QFont.Bold))
+        self.main_text.move(250, 100)
+        self.main_text.adjustSize()
+
+        self.btn_startplay = QtWidgets.QPushButton(self)
+        self.btn_startplay.move(100, 200)
+        self.btn_startplay.setText("Play")
+        self.btn_startplay.setFixedWidth(200)
+
+        self.btn_exit = QtWidgets.QPushButton(self)
+        self.btn_exit.move(100, 250)
+        self.btn_exit.setText("Exit")
+#        self.btn_exit.clicked.connect(self.exit_main())
+        self.btn_exit.setFixedWidth(200)
+
+#    def exit_main(self):
+#       sys.exit()
 
 def application():
     app = QApplication(sys.argv)
-    window = QMainWindow()
-
-    window.setWindowTitle("Игрушка")
-    window.setGeometry(300, 250, 800, 600)
-
-    main_text = QtWidgets.QLabel(window)
-    main_text.setText("Кликомания")
-    main_text.move(400, 300)
+    window = Window()
 
     window.show()
     sys.exit(app.exec_())
-
 
 if __name__ == "__main__":
     application()
