94bf9666ff60e42658edb5a3af52c7e59925bd38
Krakazyabra21 <krakazyabra2102@mail.ru>
2022-06-04 03:56:09 +0300

    123

--- main_game.py
+++ main_game.py
@@ -4,28 +4,53 @@ from PyQt5 import QtWidgets
 from my_button import MyButton
 import random
 
+
 class MainGame(QMainWindow):
 
     def __init__(self):
         super().__init__()
+        self.clickButton = None
         self.__ui = Ui_MainWindow()
         self.__ui.setupUi(self)
         self.firstSpacer = QtWidgets.QSpacerItem(10, 10, QtWidgets.QSizePolicy.Expanding,
                                                  QtWidgets.QSizePolicy.Expanding)
         colorList = ['red', 'green', 'white', 'black', 'blue']
+        self.labelScore = {}
+        i = 0
+        for i in range(len(colorList)):
+            self.labelScore[colorList[i]] = 1
+        #labellist = ['label_black_score','label_green_score','label_white_score','']
+        self.__ui.label_red_score.setText('x' + str(0))
+        self.__ui.label_white_score.setText('x' + str(0))
+        self.__ui.label_black_score.setText('x' + str(0))
+        self.__ui.label_blue_score.setText('x' + str(0))
+        count_red = 0
+        count_white = 0
+        count_black = 0
+        count_blue = 0
+
+        #self.label_yellow_score.setText =
+        self.score = 0
+        self.mult = 1
         self.buttons = {}
-        for i in range(1, 9):
-            for j in range(1, 9):
-                #number += 1
-                color = colorList[random.randrange(0, 5)]
+        self.endClick = False
+        self.maxRange = 9
+        for i in range(1, self.maxRange):
+            label1 = QtWidgets.QLabel('')
+            label2 = QtWidgets.QLabel('')
+            self.__ui.gridLayout_4.addWidget(label1, 10, i)
+            self.__ui.gridLayout_4.addWidget(label2, i, 10)
+            for j in range(1, self.maxRange):
+                # number += 1
+                color = colorList[random.randrange(len(colorList))]
                 but_cl = False
                 button_ij = MyButton(i, j, color, but_cl, self)
-                #button_ij.setText(str(i))
+                # button_ij.setText(str(i))
                 button_ij.setFixedWidth(50)
                 button_ij.setFixedHeight(50)
-                button_ij.setStyleSheet('background-color:'+color)
+                button_ij.setStyleSheet('background-color:' + color)
                 button_ij.clicked.connect(self.buttonIJClick)
-                #button_ij_id = str(i) + '_' + str(j)
+                # button_ij_id = str(i) + '_' + str(j)
                 self.__ui.gridLayout_4.addWidget(button_ij, i, j)
                 self.buttons[i, j] = button_ij
 
@@ -42,50 +67,99 @@ class MainGame(QMainWindow):
         self.show()
 
     def buttonIJClick(self):
-        #print(self.sender().x, self.sender().y)
+
+        # print(self.sender().x, self.sender().y)
         x = self.sender().x
         y = self.sender().y
+        if self.endClick == False:
+            self.clickButton = (x, y)
+            self.endClick = True
+
         color = self.sender().color
-
-        if (x > 1):
-            b = self.buttons[x-1, y]
+        self.sender().but_cl = True
+        if (x - 1, y) in self.buttons:
+            b = self.buttons[x - 1, y]
             print(self.sender().x, self.sender().y)
-            if color == b.color:
-                self.sender().but_cl = True
-                '''
-                if d not in press:
-                    press[H_press] = {d: {b.x, b.y}}
-                    H_press += 1
-                    d += 1
-                    #b.click()
-                    print(press)
-                
-                '''
-        '''
-        if (y > 1):
+            if (color == b.color) and (b.but_cl == False):
+                b.click()
+                self.mult += 1.5
+        if (x, y - 1) in self.buttons:
             b = self.buttons[x, y - 1]
-            if color == b.color:
+            print(self.sender().x, self.sender().y)
+            if (color == b.color) and (b.but_cl == False):
                 b.click()
+                self.mult += 1.5
 
-        if (x < 8):
+        if (x + 1, y) in self.buttons:
             b = self.buttons[x + 1, y]
-            if color == b.color:
+            print(self.sender().x, self.sender().y)
+            if (color == b.color) and (b.but_cl == False):
                 b.click()
+                self.mult += 1.5
 
-        if (y < 8):
+        if (x, y + 1) in self.buttons:
             b = self.buttons[x, y + 1]
-            if color == b.color:
+            print(self.sender().x, self.sender().y)
+            if (color == b.color) and (b.but_cl == False):
                 b.click()
-        '''
-        #'''
-        for i in range(1, 9):
-            for j in range(1,9):
-               # print(i, j)
-                if self.sender().but_cl == True:
-                     self.__ui.gridLayout_4.removeWidget(self)
-                     del self.buttons[i, j]
-            #print('Удалён: ' + self.sender().color)
-       # for i in range(1, 9):
-       #     for j in range(1, 9):
-#'''
+                self.mult += 1.5
 
+        self.__ui.gridLayout_4.removeWidget(self.sender())
+        print('Удалён: ' + self.sender().color)
+        self.labelScore[self.sender().color] += 1
+        del self.buttons[self.sender().x, self.sender().y]
+
+        if self.clickButton == (x, y):
+            self.endClick = False
+
+            for k in range(1, self.maxRange*2):
+                for i in range(1, self.maxRange - 1):
+                    for j in range(1, self.maxRange):
+                        if ((i, j) in self.buttons) and ((i+1, j) not in self.buttons):
+                            self.buttons[i+1, j] = self.buttons[i, j]
+                            self.buttons[i + 1, j].x += 1
+                            self.__ui.gridLayout_4.addWidget(self.buttons[i + 1, j], i + 1, j)
+                            #self.__ui.gridLayout_4.removeWidget(self.buttons[i, j])
+                            del self.buttons[i, j]
+
+            isColumn = {}
+            for i in range(1, self.maxRange):
+                for j in range(1, self.maxRange):
+                    if (i, j) in self.buttons:
+                        isColumn[j] = True
+            for i in range(1, self.maxRange):
+                for j in range(1, self.maxRange):
+                    if (i, j) in self.buttons:
+                        l = 0
+                        for k in range(1, j):
+                            if k not in isColumn:
+                                l += 1
+                        if l > 0:
+                            self.buttons[i, j - l] = self.buttons[i, j]
+                            self.buttons[i, j - l].y -= l
+                            self.__ui.gridLayout_4.addWidget(self.buttons[i, j - l], i, j-l)
+                            del self.buttons[i, j]
+            self.labelScoreUpdate()
+            self.mult = 1
+
+
+                        #self.__ui.gridLayout_4.addItem(self.__ui.gridLayout_4.itemAtPosition(i, j), i + 1, j)
+                    #    self.__ui.gridLayout_4.removeItem(self.__ui.gridLayout_4.itemAtPosition(i, j))
+
+    def labelScoreUpdate(self):
+        self.score += 100 * self.mult
+        self.__ui.label_score.setText(str(self.score))
+        #'''
+        if self.labelScore["red"] != 0:
+            self.__ui.label_red_score.setText('x' + str(self.labelScore["red"]) + 'red')
+        if self.labelScore["white"] != 0:
+            self.__ui.label_white_score.setText('x' + str(self.labelScore["white"]) + 'white')
+        if self.labelScore["black"] != 0:
+            self.__ui.label_black_score.setText('x' + str(self.labelScore["black"]) + 'black')
+        if self.labelScore["blue"] != 0:
+            self.__ui.label_blue_score.setText('x' + str(self.labelScore["blue"]) + 'blue')
+        if self.labelScore["green"] != 0:
+            self.__ui.label_green_score.setText('x' + str(self.labelScore["green"])  + 'green')
+
+
+# '''
