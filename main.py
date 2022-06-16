ff95921b76b01cdfafe9b66fe5480c387b61238f
Krakazyabra21 <krakazyabra2102@mail.ru>
2022-06-16 15:45:44 +0300

    complete

--- main.py
+++ main.py
@@ -6,46 +6,113 @@ from main_game import MainGame
 
 class Window(QMainWindow):
     def __init__(self):
-        x_main = 800
-        y_main = 600
+        self.list_mas = [' ', '640x480', '800x600', '1280x800']
+        self.x_main = 640
+        self.y_main = 480
         super(Window, self).__init__()
         self.setWindowTitle("Игрушка")
-        self.setFixedSize(x_main, y_main)
+        self.setFixedSize(self.x_main, self.y_main)
        #self.setGeometry(300, 250, x_main, y_main)
 
         self.main_text = QtWidgets.QLabel(self)
         self.main_text.setText("Кликомания")
         self.main_text.setFont(QtGui.QFont("Times", 32, QtGui.QFont.Bold))
-        self.main_text.move(250, 100)
+        self.main_text.move(self.x_main//2-140, 75)
         self.main_text.adjustSize()
 
         self.btn_startplay = QtWidgets.QPushButton(self)
-        self.btn_startplay.move(x_main//2 - 100, y_main//2 - 100)
+        self.btn_startplay.move(self.x_main//2 - 100, self.y_main//2 - 100)
         self.btn_startplay.setText("Играть")
         self.btn_startplay.setFixedWidth(200)
         self.btn_startplay.setFixedHeight(50)
         self.btn_startplay.clicked.connect(self.play_main)
 
         self.btn_opt = QtWidgets.QPushButton(self)
-        self.btn_opt.move(x_main // 2 - 100, y_main // 2 - 50)
+        self.btn_opt.move(self.x_main // 2 - 100, self.y_main // 2 - 50)
         self.btn_opt.setText("Настройки")
-        #self.btn_opt.clicked.connect(self.opt_main)
+        self.btn_opt.clicked.connect(self.opt_main)
         self.btn_opt.setFixedWidth(200)
         self.btn_opt.setFixedHeight(50)
 
         self.btn_exit = QtWidgets.QPushButton(self)
-        self.btn_exit.move(x_main//2 - 100, y_main//2)
+        self.btn_exit.move(self.x_main//2 - 100, self.y_main//2)
         self.btn_exit.setText("Выход")
         self.btn_exit.clicked.connect(self.exit_main)
         self.btn_exit.setFixedWidth(200)
 
-    def exit_main(self):
-       sys.exit()
+        self.btn_bmenu = QtWidgets.QPushButton(self)
+        self.btn_bmenu.move(self.x_main - 150, self.y_main - 50)
+        self.btn_bmenu.setText("В меню")
+        self.btn_bmenu.clicked.connect(self.b_menu)
+        self.btn_bmenu.setVisible(False)
+
+        self.comb_s = QtWidgets.QComboBox(self)
+        self.comb_s.setVisible(False)
+        self.comb_s.setFixedSize(200, 30)
+        self.comb_s.move(self.x_main//2 - 100, self.y_main//2 - 30)
+        self.comb_s.addItems(self.list_mas)
+        self.lab_comb = QtWidgets.QLabel(self)
+        self.lab_comb.move(self.x_main//2 - 100, self.y_main//2 - 50)
+        self.lab_comb.setText("Выберите разрешение экрана:")
+        self.lab_comb.adjustSize()
+        self.lab_comb.setVisible(False)
+
+        self.btn_acc = QtWidgets.QPushButton(self)
+        self.btn_acc.move(self.x_main//2, self.y_main - 50)
+        self.btn_acc.setText("Принять")
+        self.btn_acc.setVisible(False)
+        self.btn_acc.clicked.connect(self.change_size)
+
+    def change_size(self):
+        id_list = self.comb_s.currentIndex()
+        if id_list == 1:
+            self.x_main = 640
+            self.y_main = 480
+            self.setFixedSize(self.x_main, self.y_main)
+        if id_list == 2:
+            self.x_main = 800
+            self.y_main = 600
+            self.setFixedSize(self.x_main, self.y_main)
+        if id_list == 3:
+            self.x_main = 1280
+            self.y_main = 800
+            self.setFixedSize(self.x_main, self.y_main)
+        self.main_text.move(self.x_main // 2 - 140, 75)
+        self.btn_startplay.move(self.x_main // 2 - 100, self.y_main // 2 - 100)
+        self.btn_opt.move(self.x_main // 2 - 100, self.y_main // 2 - 50)
+        self.btn_exit.move(self.x_main // 2 - 100, self.y_main // 2)
+        self.btn_bmenu.move(self.x_main - 150, self.y_main - 50)
+        self.comb_s.move(self.x_main // 2 - 100, self.y_main // 2 - 30)
+        self.lab_comb.move(self.x_main // 2 - 100, self.y_main // 2 - 50)
+        self.btn_acc.move(self.x_main - 150, self.y_main // 2 - 50)
 
     def play_main(self):
         self.setVisible(False)
-        self.mainWindow = MainGame()
+        self.mainWindow = MainGame(self)
 
+    def exit_main(self):
+       sys.exit()
+
+    def opt_main(self):
+        self.main_text.setVisible(False)
+        self.btn_startplay.setVisible(False)
+        self.btn_opt.setVisible(False)
+        self.btn_exit.setVisible(False)
+        self.lab_comb.setVisible(True)
+        self.btn_bmenu.setVisible(True)
+        self.comb_s.setVisible(True)
+        self.btn_acc.setVisible(True)
+
+    def b_menu(self):
+        self.setVisible(True)
+        self.main_text.setVisible(True)
+        self.btn_startplay.setVisible(True)
+        self.btn_opt.setVisible(True)
+        self.btn_exit.setVisible(True)
+        self.lab_comb.setVisible(False)
+        self.btn_bmenu.setVisible(False)
+        self.comb_s.setVisible(False)
+        self.btn_acc.setVisible(False)
 
 def application():
     app = QApplication(sys.argv)
@@ -56,4 +123,3 @@ def application():
 
 if __name__ == "__main__":
     application()
-    #123
--- main_game.py
+++ main_game.py
@@ -7,23 +7,25 @@ import random
 
 class MainGame(QMainWindow):
 
-    def __init__(self):
+    def __init__(self, parent):
         super().__init__()
         self.clickButton = None
         self.__ui = Ui_MainWindow()
         self.__ui.setupUi(self)
+        self.parent = parent
         self.firstSpacer = QtWidgets.QSpacerItem(10, 10, QtWidgets.QSizePolicy.Expanding,
                                                  QtWidgets.QSizePolicy.Expanding)
-        colorList = ['red', 'green', 'white', 'black', 'blue']
+        self.colorList = ['red', 'green', 'white', 'black', 'blue']
         self.labelScore = {}
         i = 0
-        for i in range(len(colorList)):
-            self.labelScore[colorList[i]] = 1
+        for i in range(len(self.colorList)):
+            self.labelScore[self.colorList[i]] = 0
         #labellist = ['label_black_score','label_green_score','label_white_score','']
-        self.__ui.label_red_score.setText('x' + str(0))
-        self.__ui.label_white_score.setText('x' + str(0))
-        self.__ui.label_black_score.setText('x' + str(0))
-        self.__ui.label_blue_score.setText('x' + str(0))
+        self.__ui.label_red_score.setText('x' + str(0) + ' Красный')
+        self.__ui.label_white_score.setText('x' + str(0) + '  Белый')
+        self.__ui.label_black_score.setText('x' + str(0) + ' Чёрный')
+        self.__ui.label_blue_score.setText('x' + str(0) + ' Синий')
+        self.__ui.label_green_score.setText('x' + str(0) + ' Зелёный')
         count_red = 0
         count_white = 0
         count_black = 0
@@ -32,7 +34,8 @@ class MainGame(QMainWindow):
         #self.label_yellow_score.setText =
         self.score = 0
         self.mult = 1
-        self.but_del = 1
+        self.but_del = 0
+        self.end = False
         self.buttons = {}
         self.endClick = False
         self.maxRange = 10
@@ -42,35 +45,39 @@ class MainGame(QMainWindow):
             self.__ui.gridLayout_4.addWidget(label1, 10, i)
             self.__ui.gridLayout_4.addWidget(label2, i, 10)
             for j in range(1, self.maxRange):
-                # number += 1
-                color = colorList[random.randrange(len(colorList))]
+                color = self.colorList[random.randrange(len(self.colorList))]
                 but_cl = False
                 button_ij = MyButton(i, j, color, but_cl, self)
-                # button_ij.setText(str(i))
+                #button_ij.setText(str(i) + str(j))
                 button_ij.setFixedWidth(50)
                 button_ij.setFixedHeight(50)
                 button_ij.setStyleSheet('background-color:' + color)
                 button_ij.clicked.connect(self.buttonIJClick)
-                # button_ij_id = str(i) + '_' + str(j)
+                #button_ij_id = str(i) + '_' + str(j)
                 self.__ui.gridLayout_4.addWidget(button_ij, i, j)
                 self.buttons[i, j] = button_ij
 
         self.__ui.gridLayout_4.addItem(self.firstSpacer, 0, 0, 1, 1)
-        # button1 = QtWidgets.QPushButton("1", self)
-        # self.__ui.gridLayout_4.addWidget(button1, 1, 1)
-        # button2 = QtWidgets.QPushButton("2", self)
-        # self.__ui.gridLayout_4.addWidget(button2, 2, 2)
+        #button1 = QtWidgets.QPushButton("1", self)
+        #self.__ui.gridLayout_4.addWidget(button1, 1, 1)
+        #button2 = QtWidgets.QPushButton("2", self)
+        #self.__ui.gridLayout_4.addWidget(button2, 2, 2)
 
         self.lastSpacer = QtWidgets.QSpacerItem(10, 10, QtWidgets.QSizePolicy.Expanding,
                                                 QtWidgets.QSizePolicy.Expanding)
         self.__ui.gridLayout_4.addItem(self.lastSpacer, 10, 10, 1, 1)
-
+        self.startButton = (1,2)
+        self.isStart = False
         self.show()
 
     def buttonIJClick(self):
         # print(self.sender().x, self.sender().y)
+        #self.but_del += 1
         x = self.sender().x
         y = self.sender().y
+        if self.isStart == False:
+            self.startButton = (x, y)
+            self.isStart = True
         if self.endClick == False:
             self.clickButton = (x, y)
             self.endClick = True
@@ -151,25 +158,104 @@ class MainGame(QMainWindow):
                 self.labelScoreUpdate()
             self.but_del = 0
             self.mult = 1
-
+            if (x, y) in self.buttons:
+                self.buttons[x, y].but_cl = False
+            if (self.startButton==(x, y)):
+                if len(self.buttons) == 0:
+                    self.msbox_end()
+                self.zero_click()
+                self.isStart = False
 
                         #self.__ui.gridLayout_4.addItem(self.__ui.gridLayout_4.itemAtPosition(i, j), i + 1, j)
                     #    self.__ui.gridLayout_4.removeItem(self.__ui.gridLayout_4.itemAtPosition(i, j))
+
+    def zero_click(self):
+        self.end = False
+        for but_key in self.buttons:
+            but = self.buttons[but_key]
+            x = but.x
+            y = but.y
+            color = but.color
+            if (x - 1, y) in self.buttons:
+                b = self.buttons[x - 1, y]
+                if color == b.color:
+                    self.end = True
+
+            if (x, y - 1) in self.buttons:
+                b = self.buttons[x, y - 1]
+                if color == b.color:
+                    self.end = True
+
+            if (x + 1, y) in self.buttons:
+                b = self.buttons[x + 1, y]
+                if color == b.color:
+                    self.end = True
+
+            if (x, y + 1) in self.buttons:
+                b = self.buttons[x, y + 1]
+                if color == b.color:
+                    self.end = True
+
+        if self.end == False:
+            self.msbox_end()
+
+    def msbox_end(self):
+
+            msgBox = QtWidgets.QMessageBox()
+            msgBox.setIcon(QtWidgets.QMessageBox.Information)
+            msgBox.setText("Игра закончена. Попробовать снова?")
+            msgBox.setWindowTitle("Конец игры.")
+            msgBox.setStandardButtons(QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
+            # msgBox.buttonClicked.connect(self.msgButtonClick)
+
+            returnValue = msgBox.exec()
+            if returnValue == QtWidgets.QMessageBox.Yes:
+                self.reset()
+            else:
+                #self.Ui_MainWindow.setVisible(True)
+                self.parent.show()
+                self.close()
+
+    def reset(self):
+        for but in self.buttons:
+            self.__ui.gridLayout_4.removeWidget(self.buttons[but])
+        self.buttons.clear()
+        for i in range(1, self.maxRange):
+            for j in range(1, self.maxRange):
+                # number += 1
+                color = self.colorList[random.randrange(len(self.colorList))]
+                but_cl = False
+                button_ij = MyButton(i, j, color, but_cl, self)
+                # button_ij.setText(str(i))
+                button_ij.setFixedWidth(50)
+                button_ij.setFixedHeight(50)
+                button_ij.setStyleSheet('background-color:' + color)
+                button_ij.clicked.connect(self.buttonIJClick)
+                # button_ij_id = str(i) + '_' + str(j)
+                self.__ui.gridLayout_4.addWidget(button_ij, i, j)
+                self.buttons[i, j] = button_ij
+
+                self.__ui.label_red_score.setText('x' + str(0) + ' Красный')
+                self.__ui.label_white_score.setText('x' + str(0) + '  Белый')
+                self.__ui.label_black_score.setText('x' + str(0) + ' Чёрный')
+                self.__ui.label_blue_score.setText('x' + str(0) + ' Синий')
+                self.score = 0
+                self.__ui.label_score.setText(str(self.score))
 
     def labelScoreUpdate(self):
         self.score += 100 * self.mult
         self.__ui.label_score.setText(str(self.score))
         #'''
         if self.labelScore["red"] != 0:
-            self.__ui.label_red_score.setText('x' + str(self.labelScore["red"]) + 'red')
+            self.__ui.label_red_score.setText('x' + str(self.labelScore["red"]) + ' Красный')
         if self.labelScore["white"] != 0:
-            self.__ui.label_white_score.setText('x' + str(self.labelScore["white"]) + 'white')
+            self.__ui.label_white_score.setText('x' + str(self.labelScore["white"]) + ' Белый')
         if self.labelScore["black"] != 0:
-            self.__ui.label_black_score.setText('x' + str(self.labelScore["black"]) + 'black')
+            self.__ui.label_black_score.setText('x' + str(self.labelScore["black"]) + ' Чёрный')
         if self.labelScore["blue"] != 0:
-            self.__ui.label_blue_score.setText('x' + str(self.labelScore["blue"]) + 'blue')
+            self.__ui.label_blue_score.setText('x' + str(self.labelScore["blue"]) + ' Синий')
         if self.labelScore["green"] != 0:
-            self.__ui.label_green_score.setText('x' + str(self.labelScore["green"])  + 'green')
+            self.__ui.label_green_score.setText('x' + str(self.labelScore["green"]) + ' Зелёный')
 
 
 # '''
